950538144c7b524e052d950e83df65c7e5ee6e9f
SantiagoDevRel <santiago@chainsafe.io>
2024-02-03 17:36:37 +0000

    update usage

--- deployContracts/usage.js
+++ deployContracts/usage.js
@@ -5,7 +5,7 @@ const bytecode = require("./bytecode.json");
 
 const plugin = new DeployContracts();
 
-const privateKey = "0xca0f203073b871007e1b050d2d318e073816078d7c1a04d73baee3cad3127cdf";
+const privateKey = "0x...";
 
 async function main() {
   await plugin.deploySepolia(abi, bytecode, privateKey);
